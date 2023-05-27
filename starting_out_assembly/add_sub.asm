b4be628a033c5ba8317a2f281f99dd9482e64ce9
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-05-26 17:59:05 -0700

    hard coded sum

--- starting_out_assembly/add_sub.asm
+++ starting_out_assembly/add_sub.asm
@@ -1,3 +1,7 @@
+; this program receives 2 numbers from the user
+; and display their sum
+
+; define the constants for system calls
 SYS_EXIT EQU 1
 SYS_READ EQU 3
 SYS_WRITE EQU 4
@@ -6,47 +10,52 @@ STDOUT EQU 1
 STDERR EQU 2
 
 section .data 
-    msg1 db 'Enter a digit: '
-    len1 equ $-msg1
+    msg1 db 'Enter a digit: '   ; first prompt
+    len1 equ $-msg1     ; its length
 
-    msg2 db "Please enter a second digit: "
-    len2 equ $-msg2
+    msg2 db "Please enter a second digit: " ; second prompt
+    len2 equ $-msg2     ; its length
 
-    msg3 db "The sum is : "
-    len3 equ $-msg3
+    msg3 db "The sum is : "     ; result prompt
+    len3 equ $-msg3     ; its length
 
 section .bss
-    num1 resb 2
-    num2 resb 2
-    res resb 2
+    num1 resb 2         ; reserve 2 byte for first number
+    num2 resb 2         ; same for second number
+    res resb 2          ; reserve for result
 
 section .text
-    global _start
+    global _start       ; for linker
 _start:
+    ; display first prompt
     mov eax, SYS_WRITE
     mov ebx, STDOUT
     mov ecx, msg1
     mov edx, len1
     int 0x80
 
+    ; read first number
     mov eax, SYS_READ
     mov ebx, STDIN
     mov ecx, num1
     mov edx, 2
     int 0x80
 
+    ; display second prompt
     mov eax, SYS_WRITE
     mov ebx, STDOUT
     mov ecx, msg2
     mov edx, len2
     int 0x80
 
+    ; read second number
     mov eax, SYS_READ
     mov ebx, STDIN
     mov ecx, num2
     mov edx, 2
     int 0x80
 
+    ; display result prompt
     mov eax, SYS_WRITE
     mov ebx, STDOUT
     mov ecx, msg3
@@ -75,6 +84,7 @@ _start:
     mov edx, 2
     int 0x80
 
+; exit the program
 exit:
     mov eax, SYS_EXIT
     xor ebx, ebx
---
+++ starting_out_assembly/hardcode_sum.asm
@@ -0,0 +1,30 @@
+section .text
+    global _start     ; must be declared for linker ld
+_start:
+    mov eax, '3'
+    sub eax, '0'
+    mov ebx, '4'
+    sub eax, '0'
+    add eax, ebx
+    add eax, '0'
+    mov [sum], eax
+    mov ecx, msg
+    mov edx, len
+    mov ebx, 1      ; stdout
+    mov eax, 4      ; system write
+    int 80h
+
+    mov ecx, sum
+    mov edx, 1
+    mov ebx, 1
+    mov eax, 4
+    int 80h
+
+    mov eax, 1
+    int 80h
+
+section .data
+    msg db "The sum is: "
+    len equ $-msg
+section .bss
+    sum resb 1
